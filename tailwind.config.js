a9ba8659b7a347629b14e4d91214b2401f289a6b
imam <34465856+aimamadyan@users.noreply.github.com>
2023-03-16 15:08:07 +0700

    revisi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            'ungu': '#4D455D',
+            'merah': '#E96479',
+            'krem': '#F5E9CF',
+            'biru': '#7DB9B6'
+        },
+    },
   },
   plugins: [],
 }
